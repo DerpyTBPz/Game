104e9c26d721171cf3de0cbcef03dcbeb596f9a5
DerpyTBPz <dima.plyasov13@gmail.com>
2022-02-09 09:17:05 +0200

    simple enemy

--- Game/Assets/Scripts/Bullet.cs
+++ Game/Assets/Scripts/Bullet.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed;
+    public int damage;
     public float startLifetime;
     private float lifetime;
     public float distance;
+    public LayerMask whatIsSolid;
 
     void Start()
     {
@@ -16,7 +18,17 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("hit");
+                hitInfo.collider.GetComponent<Enemy>().takeDamage(damage);
+                Destroy(gameObject);
+            }
+        }
+        transform.Translate(Vector2.up * speed * Time.deltaTime);  
         destroyBullet();
     }
 
