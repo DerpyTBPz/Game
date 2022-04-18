6cc102422921b042032cd0e64e79d730018d6e5a
DerpyTBPz <dima.plyasov13@gmail.com>
2022-04-18 12:20:51 +0300

    dynamicCamera

---
+++ Game/Assets/Scripts/playerCamera.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+
+    private Transform playerTransform;
+    private string playerTag;
+    public float movingSpeed; 
+    Vector3 mousePosition;
+
+
+    private void Awake()
+    {
+        if (this.playerTransform == null)
+        {            
+            this.playerTag = "Player";
+
+            this.playerTransform = GameObject.FindGameObjectWithTag(this.playerTag).transform;
+        }    
+
+        mousePosition = Input.mousePosition; 
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);       
+
+        this.transform.position = new Vector3()
+        {
+            x = this.playerTransform.position.x,
+            y = this.playerTransform.position.y,
+            z = this.playerTransform.position.z - 10,
+        };
+    }
+    
+    private void Update() 
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        if (this.playerTransform)
+        {           
+           
+            Vector3 target = new Vector3()
+            {
+                x = this.playerTransform.position.x,
+                y = this.playerTransform.position.y,
+                z = this.playerTransform.position.z - 10,
+            };
+
+            Vector3 pos = Vector3.Lerp(this.transform.position, target, this.movingSpeed = Time.deltaTime);
+
+            this.transform.position = pos;
+        }
+        
+    }
+
+}
